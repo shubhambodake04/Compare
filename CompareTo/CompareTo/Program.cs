49774bb335da6a4c82781f012ae7ee3dafe3f83d
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-21 10:20:15 +0530

    [Shubham] added CompareTo

---
+++ CompareTo/CompareTo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompareTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String A = "ABC";
+            String B = "ABC";
+            int result = 0;
+
+            result = String.Compare(A, B);
+            Console.WriteLine(+result);
+            
+        }
+    }
+}
